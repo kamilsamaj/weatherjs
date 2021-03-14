a9ec73b0902bc999ffca5d716687c46b4aa09511
kamilsamaj <kamilsamaj@gmail.com>
2021-03-14 19:07:41 +0100

    add fetch handler

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-const weatherClient = new Weather();
+const weather = new Weather('Prague', 'CZ');
+
+weather.getWeather()
+    .then(results => {
+        console.log(results);
+    })
+    .catch(err => console.error(err));
--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,42 @@
 class Weather {
-    constructor() {
-        this._apitoken = "";
-        this._loadApiKey().then((token) => {
+    #apitoken = "";
+    #weatherApi = "https://api.openweathermap.org/data/2.5";
+
+    constructor(city, country) {
+        this._loadApiKey().then(() => {
             console.debug('Token loaded');
-            this._apitoken = token;
+
         });
+        this.city = city;
+        this.country = country;
+    }
+
+    async _getCall(url) {
+        console.log(url);
+        if (this.#apitoken === "") {
+            await this._loadApiKey();
+        }
+        // add the 'appid' QS params
+        if (url.includes('?') === true) {
+            url += `&appid=${this.#apitoken}`;
+        } else {
+            url += `?appid=${this.#apitoken}`;
+        }
+
+        console.log(this.#apitoken);
+        return await fetch(url);
+    }
+
+    // Fetch weather from API
+    async getWeather() {
+        const response = await this._getCall(`${this.#weatherApi}/weather?q=${this.city},${this.country}`);
+        return await response.json();
+    }
+
+    // Update location
+    changeLocation(city, country) {
+        this.city = city;
+        this.country = country;
     }
 
     async _loadApiKey() {
@@ -13,7 +45,7 @@ class Weather {
             if (response.status !== 200) {
                 throw "Could not load the 'apitoken' local file";
             }
-            return await response.text();
+            this.#apitoken = await response.text();
         } catch (e) {
             console.error(e);
         }
